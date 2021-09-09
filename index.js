f1b1576ff1ac93adc285ee2ed0e4c39b4d21280b
Kevin Tran <kevin_tran95@yahoo.com>
2021-09-08 22:06:35 -0700

    added manager questions

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const fs = require('fs')
 const inquirer = require("inquirer")
 
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// Manager questions
+const managerInput = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your team managers name?',
+        },
+        {
+            type:'input',
+            name:'id',
+            message:'What is your team managers id?',
+        },
+        {
+            type:'input',
+            name: 'email',
+            message:'What is your team managers email?',
+        },
+        {
+            type:'input',
+            name:'officeNumber',
+            message: 'What is your team managers office number?',
+        }
+    ])
+}
